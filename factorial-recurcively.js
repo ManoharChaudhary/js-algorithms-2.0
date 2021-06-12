a39ff10805650f4df9344f639598b9626ca82761
Manohar Chaudhari <mchaudhari@mediaocean.com>
2021-06-12 11:50:06 +0530

    Recursion: Factorial using recursion.

---
+++ factorial-recurcively.js
@@ -0,0 +1,32 @@
+/**
+ * Recursion basics - using factorial
+ */
+
+
+/**
+ * Factorial of give number
+ * @param {number} n - param of factorial
+ * @returns {number} - factorial of n
+ */
+
+const factorial = (n) => {
+    //console.log("number n value", n);
+    if (n === 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+    //const f = n * factorial(n - 1);
+    //console.log("factorial value", f);
+    //return f;
+}
+factorial(4);
+
+/**program tracing
+ *  function    return      value
+ *  f(4)        4 * f(3)    24
+ *  f(3)        3 * f(2)    6
+ *  f(2)        2 * f(1)    2
+ *  f(1)        1 * f(0)    1
+ *  f(0)        1           1
+ *
+ */
