1f78a741c3f7c289f404b28ae710b15cfe7558ce
Manohar Chaudhari <mchaudhari@mediaocean.com>
2021-06-14 16:21:52 +0530

    Math basics: Decimal to Binary.

---
+++ decimal-to-binary.js
@@ -0,0 +1,28 @@
+/**
+ * Math basics - decimal to binary
+ */
+
+
+/**
+ * Decimal to binary 
+ * @param {number} n - param of base10 number
+ * @returns {number} - binary number
+ */
+
+const decimalToBinary = (n) => {
+    let binary = '';
+    while (n > 0) {
+        const rem = n % 2;
+        n = Math.floor(n / 2);
+        binary = rem + binary;
+    }
+    return binary;
+}
+
+console.log(decimalToBinary(125));
+
+/**
+ * same can be done using toString(2) method
+ * * time complexity O(n)
+ * space complexity O(n)
+ */
