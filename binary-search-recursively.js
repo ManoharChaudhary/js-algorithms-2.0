d2b3f4349b63c9e1b327723ae8ff3cf2ca857474
Manohar Chaudhari <manohar.chaudhari@outlook.com>
2021-06-16 22:00:58 +0530

    Binary Search: Iterative

--- binary-search-recursively.js
+++ binary-search-recursively.js
@@ -12,20 +12,19 @@
  */
 
 const binarySearch = (arr, x, start = 0, end = arr.length) => {
-    while (start <= end) {
-        const mid = Math.floor((start + end) / 2);
-        if (arr[mid] === x) {
-            return `Yes(${mid})`;
-        } else if (arr[mid] < x) {
-            start = mid + 1;
-        } else if (arr[mid] > x) {
-            end = mid - 1;
-        }
+    const mid = Math.floor((start + end) / 2);
+    if (arr[mid] === x) {
+        return `Yes(${mid})`
+    } else if (arr[mid] < x) {
+        return binarySearch(arr, x, mid + 1, end);
+    } else if (arr[mid] > x) {
+        return binarySearch(arr, x, start, mid - 1);
+    } else {
+        return `No`;
     }
-    return `No`;
 }
 
-console.log(binarySearch([-5, 5, 6, 23, 56, 66, 87, 100], 100));
+console.log(binarySearch([-5, 5, 6, 23, 56, 66, 87, 100], -5));
 
 
 
